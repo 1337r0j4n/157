6ffd6c0fb3814728347a3056478a2c106797cb04
7r0j4n <84887432+1337r0j4n@users.noreply.github.com>
2022-03-13 11:06:16 +0700

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<html>
-  <body bgcolor="white">
-<center>
-<img src="https://ih1.redbubble.net/image.1991623799.7434/st,small,507x507-pad,600x600,f8f8f8.jpg" height="500"><br>
-<font size="10" face="serif"><b>Hacked by LeetGh0sts Squ4d</b></font>
-</center>
-  </body>
- </html>
